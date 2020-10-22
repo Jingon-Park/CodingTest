cb77fa81abee2ed5a21b5600eafd44222aef50b8
Jingon-Park <bennp3038@gmail.com>
2020-10-22 23:55:17 +0900

    10-22일 백준 문제풀이
    while문 완성

---
+++ BaekJoon/src/Step_while/num_10951.java
@@ -0,0 +1,17 @@
+package Step_while;
+
+import java.util.Scanner;
+
+public class num_10951 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			System.out.println(sc.nextInt() + sc.nextInt());
+		}
+		
+	}
+
+}
---
+++ BaekJoon/src/Step_while/num_1110.java
@@ -0,0 +1,25 @@
+package Step_while;
+
+import java.util.Scanner;
+
+public class num_1110 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		int copy = input;
+		int counter = 0;
+		
+		do {
+			counter++;
+			copy = ((copy % 10)*10) + (((copy / 10) + (copy % 10)) % 10);
+			System.out.println(copy);
+		}while(copy != input);
+		System.out.println(counter);
+		
+			
+	}
+
+}
