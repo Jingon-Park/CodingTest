a99a93e39959cf6799ed13efaede551d2710780f
Park Jin Gon <bennp3038@gmail.com>
2021-08-16 16:33:35 +0900

    sort using buffer

--- BaekJoon/Strp_sort/num_2751.java
+++ BaekJoon/Strp_sort/num_2751.java
@@ -5,48 +5,24 @@ public class num_2751 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        StringBuffer bf = new StringBuffer();
+
         int count = sc.nextInt();
-        int[] input = new int[count];
+        ArrayList<Integer> input = new ArrayList<>();
 
         for(int i = 0; i < count; i++){
-            input[i] = sc.nextInt();
+            input.add(sc.nextInt());
         }
 
-        quick(input, 0, input.length - 1);
+        Collections.sort(input);
 
-        for(int i = 0; i < input.length; i++){
-            System.out.println(input[i]);
+        for(Integer num : input){
+            bf.append(num).append("\n");
         }
 
-        sc.close();
+        System.out.println(bf);
 
     }
     
-    public static void quick(int[] arr, int l, int r){
-        if(r > l){
-            int p = pivot(arr, l, r);
-
-            quick(arr, l, p - 1);
-            quick(arr, p + 1, r);
-        }
-    }
-
-    public static int pivot(int[] arr, int l, int r){
-        int pivot = arr[r];
-        int i = (l - 1);
-
-        for(int j = l; j <= r - 1; j++){
-            if(arr[j] <= pivot){
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp; 
-            }
-        }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[r];
-        arr[r] = temp;
-        
-        return i + 1;
-    }
+    
 }
