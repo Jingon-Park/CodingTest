f6828a8e6e78a600693ae5fe0e3d6268571a27b7
Park Jin Gon <bennp3038@gmail.com>
2021-08-31 14:12:09 +0900

    1427 solution

---
+++ BaekJoon/Step_sort/num_1427.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+
+public class num_1427 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+        ArrayList<Integer> parse = new ArrayList<>();
+
+        while(input / 10 > 0){
+            parse.add(input % 10);
+            input /= 10;
+        }
+        parse.add(input);
+        Collections.sort(parse, Comparator.reverseOrder());
+
+        for(int number : parse){
+            System.out.print(number);
+        }
+        sc.close();
+
+    }
+}
