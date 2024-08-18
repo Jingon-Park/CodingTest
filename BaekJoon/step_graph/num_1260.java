77ed62dc1dc789d358b93ad74348d28ec47b26d1
Park Jin Gon <bennp3038@gmail.com>
2024-08-18 10:59:03 +0900

    프로그래머스 "전화번호 목록" 문제 풀이

--- BaekJoon/step_graph/num_1260.java
+++ BaekJoon/step_graph/num_1260.java
@@ -1,70 +1,69 @@
 package step_graph;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
+class num_1260 {
 
-public class num_1260 {    static StringBuilder bfs = new StringBuilder();
-    static StringBuilder dfs = new StringBuilder();
-
-    static int num;
-    static int lineNum;
-    static int start;
-    static boolean[] checker;
+    static int N;
+    static int M;
+    static int V;
     static boolean[][] graph;
+    static boolean[] checker;
+    static StringBuilder dfsBuilder = new StringBuilder();
+    static StringBuilder bfsBuilder = new StringBuilder();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        V = sc.nextInt();
 
-        num = sc.nextInt();
-        lineNum = sc.nextInt();
+        graph = new boolean[N + 1][N + 1];
+        checker = new boolean[N + 1];
 
-        graph = new boolean[num + 1][num + 1];
-        start = sc.nextInt();
-
-        for (int i = 0; i < lineNum; i++) {
+        for(int i = 0; i < M; i++){
             int x = sc.nextInt();
             int y = sc.nextInt();
             graph[x][y] = graph[y][x] = true;
         }
-        checker = new boolean[num + 1];
-        dfs(start);
 
-        checker = new boolean[num + 1];
-        bfs(start);
+        dfs(V);
+        checker = new boolean[N + 1];
+        bfs(V);
+        System.out.println(dfsBuilder);
+        System.out.println(bfsBuilder);
 
-        System.out.println(dfs.toString());
-        System.out.println(bfs.toString());
 
 
     }
 
-    static void dfs(int start) {
-        if (!checker[start]){
-            checker[start] = true;
-            dfs.append(start + " ");
-        }else{
+    public static void dfs(int start){
+        if(checker[start]){
             return;
         }
-        for (int i = 1; i <= num; i++) {
-            if (graph[start][i]) {
+        checker[start] = true;
+        dfsBuilder.append(start + " ");
+
+        for(int i = 1; i <= N; i++){
+            if(graph[start][i] && !checker[i]){
                 dfs(i);
             }
         }
     }
 
-    static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList();
-        queue.add(start);
-        checker[start] = true;
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            bfs.append(node + " ");
-            for (int i = 1; i <= num; i++) {
-                if (graph[node][i] && !checker[i]) {
-                    queue.add(i);
-                    checker[i] = true;
+    public static void bfs(int start){
+        Queue<Integer> queue = new LinkedList<>();
+
+        if (!checker[start]){
+            queue.add(start);
+            checker[start] = true;
+            while (!queue.isEmpty()) {
+                int num = queue.poll();
+                bfsBuilder.append(num + " ");
+                for (int i = 1; i <= N; i++) {
+                    if (graph[num][i] && !checker[i]) {
+                        queue.add(i);
+                        checker[i] = true;
+                    }
                 }
             }
         }
---
+++ Programers/step_hash/Phone_number.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Phone_number {
+
+
+	static class Solution {
+		public static boolean solution(String[] phone_book) {
+
+			HashMap<String, Integer> map = new HashMap<>();
+
+			for(String num : phone_book){
+				map.put(num, 1);
+			}
+
+			for(String num : phone_book){
+				for(int i = 0; i < num.length(); i++){
+					String substr = num.substring(0, i);
+					if(map.containsKey(substr)) return false;
+				}
+			}
+
+			return true;
+		}
+	}
+
+	public static void main(String[] args) {
+		Boolean result = Solution.solution(new String[]{"119", "97674223", "1195524421" });
+
+		System.out.println(result);
+	}
+}
