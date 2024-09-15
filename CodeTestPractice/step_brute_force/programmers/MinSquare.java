0f92026366d2d1ab34e781d4d4f78c2a2b89a59b
gonee <bennp3038@gmail.com>
2024-09-15 16:29:33 +0900

    완전탐색 > 가장작은직사각형 풀에

---
+++ CodeTestPractice/step_brute_force/programmers/MinSquare.java
@@ -0,0 +1,37 @@
+package programmers;
+
+public class MinSquare {
+
+    static class Solution {
+        public int solution(int[][] sizes) {
+            int answer = 0;
+
+            int maxX = 0;
+            int maxY = 0;
+
+            for (int[] size : sizes) {
+                int x = size[0];
+                int y = size[1];
+
+                if (x > y) {
+                    maxX = Math.max(maxX, x);
+                    maxY = Math.max(maxY, y);
+                }else {
+                    maxX = Math.max(maxX, y);
+                    maxY = Math.max(maxY, x);
+                }
+            }
+
+            return maxX * maxY;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] input = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
+
+        int solution1 = solution.solution(input);
+        System.out.println(solution1);
+    }
+
+}
