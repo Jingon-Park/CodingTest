15985ec73dc43fc691b5a1da21cefca32cb8511c
gonee <bennp3038@gmail.com>
2025-02-23 15:00:18 +0900

    dfs/bfs > 아이템 줍기 문제 풀이(정밀도 향상)

--- CodeTestPractice/step_graph/programmers/ItemGet.java
+++ CodeTestPractice/step_graph/programmers/ItemGet.java
@@ -2,7 +2,6 @@ package step_graph.programmers;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import step_graph.programmers.GameMapQueue.Position;
 
 public class ItemGet {
 
@@ -26,40 +25,45 @@ public class ItemGet {
 
     static class Solution {
 
-        static int[][] map = new int[51][51];
-        static int[][] customMap = new int[51][51];
+        static int[][] map = new int[102][102];
+        static int[][] customMap = new int[102][102];
         static int[] moveX = {1, 0, -1, 0, 1, -1, 1, -1};
         static int[] moveY = {0, 1, 0, -1, 1, -1, -1, 1};
-        static boolean[][] visited = new boolean[51][51];
+        static boolean[][] visited = new boolean[102][102];
 
 
 
         public int solution(int[][] rectangle, int characterX, int characterY, int itemX,
             int itemY) {
             int answer = 0;
+            characterX *= 2;
+            characterY *= 2;
+            itemX *= 2;
+            itemY *= 2;
 
             for (int i = 0; i < rectangle.length; i++) {
 
-                int leftX = rectangle[i][0];
-                int leftY = rectangle[i][1];
-                int rightX = rectangle[i][2];
-                int rightY = rectangle[i][3];
+                int leftX = rectangle[i][0] * 2;
+                int leftY = rectangle[i][1] * 2;
+                int rightX = rectangle[i][2] * 2;
+                int rightY = rectangle[i][3] * 2;
 
                 for (int x = leftX; x <= rightX; x++) {
                     for (int y = leftY; y <= rightY; y++) {
-                        map[x][y] = 1;
+                        map[x][y] = 2;
                     }
                 }
             }
 
-            for (int y = 0; y <= 50; y++) {
-                for (int x = 0; x <= 50; x++) {
-                    if (map[x][y] == 1) {
+            for (int y = 0; y <= 101; y++) {
+                for (int x = 0; x <= 101; x++) {
+                    if (map[x][y] == 2) {
                         for (int i = 0; i < 8; i++) {
                             int nextX = x + moveX[i];
                             int nextY = y + moveY[i];
-                            if (nextX >= 0 && nextX <= 50 && nextY >= 0 && nextY <= 50) {
+                            if (nextX >= 0 && nextX <= 101 && nextY >= 0 && nextY <= 101) {
                                 //TODO customMap을 생성했지만, 인풋 예제 1의 3,5와 3,6은 서로 연결된 외각선이 아닌데 연결된 외각선으로 판단 중
+                                //위 문제를 해결하기 위해 index를 2배로 늘려 그래프의 정밀도를 올림
                                 if (map[nextX][nextY] == 0) {
                                     customMap[x][y] = 1;
                                     break;
@@ -70,13 +74,6 @@ public class ItemGet {
                 }
             }
 
-            for (int y = 50; y >= 0; y--) {
-                for (int x = 0; x <= 50; x++) {
-                    System.out.print(customMap[x][y]);
-                }
-                System.out.println("");
-            }
-
             Position startPosition = new Position(characterX, characterY, 0);
             Queue<Position> queue = new LinkedList<>();
 
@@ -85,17 +82,16 @@ public class ItemGet {
 
             while (!queue.isEmpty()) {
                 Position currentPosition = queue.poll();
-                System.out.println(currentPosition);
 
 
                 if (currentPosition.x == itemX && currentPosition.y == itemY) {
-                    return currentPosition.moved;
+                    return currentPosition.moved / 2;
                 }
 
                 for (int i = 0; i < 4; i++) {
                     int nextX = currentPosition.x + moveX[i];
                     int nextY = currentPosition.y + moveY[i];
-                    if (nextX >= 0 && nextX <= 50 && nextY >= 0 && nextY <= 50
+                    if (nextX >= 0 && nextX <= 101 && nextY >= 0 && nextY <= 101
                         && customMap[nextX][nextY] == 1 && !visited[nextX][nextY]) {
                         Position nextPosition = new Position(nextX, nextY, currentPosition.moved + 1);
                         queue.add(nextPosition);
