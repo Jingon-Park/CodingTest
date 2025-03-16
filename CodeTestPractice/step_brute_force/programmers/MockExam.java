b2d400361540772ab83b7a5cdd8cde31c20712e8
gonee <bennp3038@gmail.com>
2025-03-16 15:58:43 +0900

    완전탐색 -> 모의 고사 문제 풀이 ver 2

--- CodeTestPractice/step_brute_force/programmers/MockExam.java
+++ CodeTestPractice/step_brute_force/programmers/MockExam.java
@@ -1,51 +1,53 @@
 package programmers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * 완전 탐색 / 모의 시험
  */
 public class MockExam {
+
     static class Solution {
+
+        int[] num1 = {1, 2, 3, 4, 5};
+        int[] num2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] num3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         public int[] solution(int[] answers) {
+
             List<Integer> answer = new ArrayList<>();
+            int counter1 = check(answers, num1);
+            int counter2 = check(answers, num2);
+            int counter3 = check(answers, num3);
 
-            int[] pattern1 = {1, 2, 3, 4, 5};
-            int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
-            int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+            int max = Math.max(Math.max(counter1, counter2), counter3);
 
-            int[] answerNums = {getAnswerNum(pattern1, answers), getAnswerNum(pattern2,answers), getAnswerNum(pattern3, answers)};
-
-            int max = Arrays.stream(answerNums).max().getAsInt();
-
-            for (int i = 0; i < answerNums.length; i++) {
-                if (answerNums[i] == max) {
-                    answer.add(i + 1);
-                }
+            if (counter1 == max) {
+                answer.add(1);
             }
+
+            if (counter2 == max) {
+                answer.add(2);
+            }
+
+            if (counter3 == max) {
+                answer.add(3);
+            }
+
             return answer.stream().mapToInt(Integer::intValue).toArray();
         }
-    }
 
-    public static int getAnswerNum(int[] pattern, int[] answers) {
+        public int check(int[] answers, int[] values) {
+            int counter = 0;
+            for (int i = 0; i < answers.length; i++) {
+                int index = i % values.length;
+                if (answers[i] == values[index]) {
+                    counter++;
+                }
+            }
 
-        int index = 0;
-        int patternIndex = 0;
-        int answer = 0;
-        while (index < answers.length) {
-            if (answers[index] == pattern[patternIndex]) {
-                answer++;
-            }
-            index++;
-            patternIndex++;
-            if (patternIndex >= pattern.length) {
-                patternIndex = 0;
-            }
+            return counter;
         }
-
-        return answer;
     }
 
 
