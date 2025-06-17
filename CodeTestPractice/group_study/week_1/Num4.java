b323c1e90e0c2c899adf8ec9056a4ec24b75be8d
gonee <bennp3038@gmail.com>
2025-06-17 23:25:57 +0900

    그룹 스터디 1주차 4번 던전탐험 문제 풀이 dfs로는 풀이 완료 bfs로 풀어볼것

---
+++ CodeTestPractice/group_study/week_1/Num4.java
@@ -0,0 +1,55 @@
+package group_study.week_1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Num4 {
+
+
+    static class Solution {
+
+
+        static boolean[] visited;
+        static int answer = -1;
+        public int solution(int k, int[][] dungeons) {
+
+            visited = new boolean[dungeons.length];
+            dfs(0, k, dungeons);
+            return answer;
+        }
+
+        public void dfs(int count, int k, int[][] dungeons) {
+
+            for (int i = 0; i < dungeons.length; i++) {
+                if (!visited[i] && dungeons[i][0] <= k) {
+                    visited[i] = true;
+                    dfs(count + 1, k - dungeons[i][1], dungeons);
+                    visited[i] = false;
+                }
+                answer = Math.max(answer, count);
+            }
+        }
+
+        public int bfs(int count, int k, int[][] dungeons) {
+            Queue<Integer> order = new LinkedList<>();
+
+            for ()
+            while (!order.isEmpty()) {
+
+            }
+
+            return count;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int k = 80;
+        int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
+        int result = solution.solution(k, dungeons);
+        System.out.println(result);
+
+    }
+
+}
