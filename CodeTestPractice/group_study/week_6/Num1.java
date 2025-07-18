133ba3d6757185e0e0fd5936ab63b778c195059e
gonee <bennp3038@gmail.com>
2025-07-18 22:21:20 +0900

    스터디 6주차 n^2 배열 자르기 주석 추가

--- CodeTestPractice/group_study/week_6/Num1.java
+++ CodeTestPractice/group_study/week_6/Num1.java
@@ -3,15 +3,16 @@ package group_study.week_6;
 public class Num1 {
     static class Solution {
         public int[] solution(int n, long left, long right) {
-            int size = (int)(right - left);
+            int size = (int)(right - left);//리턴할 배열 크기
             int[] answer = new int[size + 1];
-            int index = 0;
-            for(long i = left; i <= right ; i++) {
+            int index = 0;//리턴할 배열의 인덱스
+            for(long i = left; i <= right ; i++) {//1차원으로 변경한 배열의 인덱스
                 answer[index++] = getNum(n, i);
             }
             return answer;
         }
 
+        //1차원 배열의 인덱스가 원래 x행 y 열에 있는지 확인하여, 값 응닶
         public int getNum(int n, long index){
             int row = (int) (index / n);
             int col = (int) (index % n);
