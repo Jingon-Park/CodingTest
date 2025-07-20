aa6715fce631ddfa85679105bce9ef2a1a5193aa
gonee <bennp3038@gmail.com>
2025-07-20 14:57:43 +0900

    스터디 6주차 연속 펄스 부분 수열의 합 처음 문제 주석 추가

--- CodeTestPractice/group_study/week_6/Num3.java
+++ CodeTestPractice/group_study/week_6/Num3.java
@@ -11,6 +11,7 @@ public class Num3 {
             int[] plusSq = new int[sequence.length];
             int[] minusSq = new int[sequence.length];
 
+            //최초 시퀀스에 펄스 적용
             for (int i = 0; i < sequence.length; i++) {
                 if (i % 2 == 0) {
                     plusSq[i] = sequence[i];
@@ -21,15 +22,19 @@ public class Num3 {
                 }
             }
 
+            //각각 펄스의 최대 값과 현재 부분 수열의 합
             long pSqMax = Integer.MIN_VALUE;
             long mSqMax = Integer.MIN_VALUE;
             long pSum = 0;
             long mSum = 0;
+
             for (int i = 0; i < sequence.length; i++) {
 
+                //이전 부분수열의 합이 음수가 되면 버림
                 pSum = Math.max(plusSq[i], pSum + plusSq[i]);
                 mSum = Math.max(minusSq[i], mSum + minusSq[i]);
 
+                //부분 수열의 합의 최대값 확인
                 pSqMax = Math.max(pSqMax, pSum);
                 mSqMax = Math.max(mSqMax, mSum);
 
