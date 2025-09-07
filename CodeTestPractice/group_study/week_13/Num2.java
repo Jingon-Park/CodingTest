c7fe2cb13ac4258bc270a213504721f770da73ac
gonee <bennp3038@gmail.com>
2025-09-07 23:49:57 +0900

    스터디 13주차 타일 채우기 문제풀이 (실패, 답안 확인함)

---
+++ CodeTestPractice/group_study/week_13/Num2.java
@@ -0,0 +1,45 @@
+package group_study.week_13;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 타일 채우기
+ * https://www.acmicpc.net/problem/2133
+ */
+public class Num2 {
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N % 2 != 0) {
+            System.out.println(0);
+            return;
+        }
+        if (N == 2) {
+            System.out.println(3);
+            return;
+        }
+
+        int[] dp = new int[N + 1];
+
+        dp[2] = 3;
+
+        for (int i = 4; i <= N; i+=2) {
+            dp[i] = (dp[i - 2] * 3) + 2;
+
+            for (int j = i - 2; j >=4; j -=2) {
+                dp[i] += dp[i - j] * 2;
+            }
+
+        }
+
+        System.out.println(dp[N]);
+
+    }
+}
